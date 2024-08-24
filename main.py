5351248f9d82b268ebddda4e5ba373a44537b943
Shmath64 <64mdavis@gmail.com>
2024-08-24 13:31:27 -0400

    Fetching analysis ID

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import Final #Allows gradual (static & dynamic) typing
 import os
 from responses import get_response
 from dotenv import load_dotenv
@@ -6,7 +5,7 @@ from discord import Intents, Client, Message
 
 #Load token
 load_dotenv()
-TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+TOKEN: str = os.getenv('DISCORD_TOKEN')
 print(TOKEN)
 
 #Bot Setup
@@ -19,17 +18,14 @@ client: Client = Client(intents=intents)
 #Message Functionality:
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message:
-        print('Message was empty likely because intents were not enabled')
+        print('Message was empty - likely because intents were not enabled')
         return
-    
-    if is_private := user_message[0] == "?":
-        user_message = user_message[1:]
 
     try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        response: str = await get_response(user_message)
+        if response: await message.reply(response)
     except Exception as e:
-        print(e)
+        raise e
 
 #Handle Bot Startup
 @client.event
--- responses.py
+++ responses.py
@@ -1,11 +1,47 @@
-from random import choice, randint
+import re
+import os
+import requests
+import json
+from dotenv import load_dotenv
 
-def get_response(user_input: str) -> str:
-    lowered: str = user_input.lower()
+load_dotenv()
+VT_API_KEY: str = os.getenv('VT_API_KEY')
 
-    if lowered == '':
-        return 'Silent'
-    elif 'hello' in lowered:
-        return 'Hello there!'
-    else:
-        return choice(["a", "b", "c"])
+url_scanner_url = "https://www.virustotal.com/api/v3/urls"
+
+payload = { "url": "wikipedia.org" }
+headers = {
+    "accept": "application/json",
+    "x-apikey": VT_API_KEY,
+    "content-type": "application/x-www-form-urlencoded"
+}
+
+
+#regex for detecting URLs was taken from GeeksForGeeks (may not be the option) https://www.geeksforgeeks.org/python-check-url-string/
+url_regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+
+
+async def get_response(user_response: str) -> str:
+    lowered_input: str = user_response.lower()
+
+    if urls := check_for_urls(lowered_input):
+        print("In get_response: URL detected")
+        response = await requests.post(url_scanner_url, data={"url":urls[0]}, headers=headers) #Just scans first URL for now
+        return get_id_from_response(response.text)
+    return 
+    
+def check_for_urls(input: str) -> list:
+    urls = re.findall(url_regex, input)
+    return [url[0] for url in urls] #this is necessary because re.findall returns a list of tuples
+
+if __name__ == "__main__":
+    print("HEADERS: " + headers)
+    response = requests.post(url_scanner_url, data=payload, headers=headers)
+    response_dict = json.loads(response.text)
+    analysis_id = response_dict["data"]["id"]
+    print("ID: " + analysis_id)
+
+def get_id_from_response(response_text: str) -> str:
+    response_dict = json.loads(response_text)
+    analysis_id = response_dict["data"]["id"]
+    return analysis_id
