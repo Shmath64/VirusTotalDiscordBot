19a74bf4a130894333dd509388bed534f71f2e39
Shmath64 <64mdavis@gmail.com>
2024-08-24 15:07:21 -0400

    Added Polling loop to fix premature replies

--- responses.py
+++ responses.py
@@ -3,13 +3,16 @@ import os
 import requests
 import json
 import aiohttp
+import asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
 VT_API_KEY: str = os.getenv('VT_API_KEY')
+#Format of link: https://www.virustotal.com/gui/url-analysis/u-23fbe66ef86597b90100fe0a34e534763c4773a88b79e97320c637a4aa8e99b4-1724526182
 
 url_scanner_url = "https://www.virustotal.com/api/v3/urls"
 analysis_url = "https://www.virustotal.com/api/v3/analyses/"
+url_analysis_gui_url = "https://www.virustotal.com/gui/url-analysis/"
 
 payload = { "url": "wikipedia.org" }
 headers = {
@@ -29,17 +32,25 @@ async def get_response(user_response: str) -> str:
     if urls := check_for_urls(lowered_input):
         print("In get_response: URL detected")
 
-        #Using 'aiohttp' instead of 'requests' to keep it asynchronous 
+        # Using 'aiohttp' instead of 'requests' to keep it asynchronous
         async with aiohttp.ClientSession() as session:
             async with session.post(url_scanner_url, data={"url":urls[0]}, headers=headers) as response:
                 response_data = await response.json()
                 analysis_id = get_id_from_response(response_data)
                 print(analysis_id)
-                
-                async with session.get(f'{analysis_url}{analysis_id}', headers=headers) as analysis_response:
-                    analysis_result = await analysis_response.json()
-                    print(analysis_result["data"]["attributes"]["stats"])
-                    return str(analysis_result["data"]["attributes"]["stats"])
+
+                # Polling loop: wait until the analysis is complete
+                while True:
+                    async with session.get(f'{analysis_url}{analysis_id}', headers=headers) as analysis_response:
+                        analysis_result = await analysis_response.json()
+                        status = analysis_result["data"]["attributes"]["status"]
+                        if status == "completed":
+                            print("Analysis completed.")
+                            print(analysis_result["data"]["attributes"]["stats"])
+                            return str(analysis_result["data"]["attributes"]["stats"]) #+ f'{url_analysis_gui_url}{analysis_id}'
+                        else:
+                            print(f"Analysis status: {status}. Waiting for completion...")
+                            await asyncio.sleep(5)  # wait 5 seconds before checking again
     
 def check_for_urls(input: str) -> list:
     urls = re.findall(url_regex, input)
